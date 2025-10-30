989af14f2bd927b2825895b53db4704514dd5d02
Olivia Knight <olivialyleknight@gmail.com>
2025-10-30 12:57:38 -0400

    Montly package update

--- src/Commands/StatsCommand.js
+++ src/Commands/StatsCommand.js
@@ -12,7 +12,7 @@ module.exports = {
 		//Database Collection Vars
 		let bot_data = databaseCollections.bot_data;
 		//Updated Date
-		let updatedDate = '9/05/2025'
+		let updatedDate = '10/30/2025'
 		//Memory Math
 		function getMemoryArray (){
 			return client.shard.broadcastEval(async (c) => {
